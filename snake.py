d32159a87ac0d434135bc6a8584304a7401e87b2
AnnaMariaShoshmina <43200698+AnnaMariaShoshmina@users.noreply.github.com>
2018-10-17 09:38:00 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,19 +1,19 @@
-      #!/usr/bin/env python3
-      #snakefunction
-      # -*- coding: utf-8 -*-
-  
-      import math
-      #из библиотеки math, numpy, matplotlib
-      import numpy
-      import matplotlib.pyplot as mpp
+#!/usr/bin/env python3
+#snakefunction
+# -*- coding: utf-8 -*-
+
+import math
+#из библиотеки math, numpy, matplotlib
+import numpy
+import matplotlib.pyplot as mpp
  
-        if __name__=='__main__':
-            #задай параметры синусоиды
-        arguments = numpy.r_[0:200:0.1]
-        mpp.plot(
-           arguments,
-           [math.sin(a) * math.sin(a/20.0) for a in arguments]
-       )
-      mpp.show() #выведи синусоиду
+if __name__=='__main__': #задай параметры синусоиды
+    arguments = numpy.r_[0:200:0.1]
+    mpp.plot(
+        arguments,
+        [math.sin(a) * math.sin(a/20.0) for a in arguments]
+    )
+    mpp.show() #выведи синусоиду
+
 
    
