0ef1ea9e20aedbe702245764899f28d34af9eacc
AnnaMariaShoshmina <43200698+AnnaMariaShoshmina@users.noreply.github.com>
2018-11-21 10:06:50 +0300

    Update simpleornot.py

--- simpleornot.py
+++ simpleornot.py
@@ -2,10 +2,9 @@ import functools
 import math
 @functools.lru_cache()
 def is_simple_cached(value):
-    flag = 1
-    i = 2
-    for i in range (int(math.sqrt(value))):
-        if value % i:
-            flag = 0
-    return flag
-print (is_simple_cached (217))
+    for i in range (2, 1+math.ceil(math.sqrt(value))):
+        if value % i == 0:
+            return False
+    return True
+for i in range(1,100):
+    print (i,is_simple_cached (i))
